7dbf6523d8d2c16c71672bb2241c1d9e01d9f189
Jade Manzur <jade.almeida@dcomp.sor.ufscar.br>
2020-09-29 17:56:01 -0300

    Initial commit

---
+++ ProjParcial1/projetoparcial1.m
@@ -0,0 +1,13 @@
+%Projeto Parcial 1 - Processamento de Imagens
+%Bruno Frítoli Carrazza - RA: 770993
+%Jade Manzur de Almeida - RA: 771025
+%------------------------------------------------------
+close all; %fecha todas as janelas abertas
+clear;     %limpa a memóra
+clc;       %limpa a área de trabalho
+%------------------------------------------------------
+%----------------------AUTOESCALA----------------------
+%----------------------QUANTIZAÇÃO---------------------
+%----------------------SPLITTING-----------------------
+%----------------------EQUALIZAÇÃO---------------------
+%----------------------BINARIZAÇÃO---------------------
