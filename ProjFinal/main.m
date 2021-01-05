4d03b91eae6fb6ad7f21953bc447d69a191e3981
Jade Manzur <jade.almeida@dcomp.sor.ufscar.br>
2021-01-05 16:40:08 -0300

    added subplots

--- ProjFinal/main.m
+++ ProjFinal/main.m
@@ -1,7 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Trabalho Final de Processamento de Imagens %
+% Implementação por:                         %
+% Bruno Carraza e Jade Manzur                %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%limpa a tela
 clear all 
 close all
 clc
-
+%importa a biblioteca de imagens
 pkg load image
 
 
@@ -10,27 +17,29 @@ function output =  ela(img, compression)
   %implementação de Error Level Analysis
   %salva a imagem em jpg, gerando compressao
 
-  imwrite(img,'tmp.jpg','Quality',compression);
-  img_ela = imread('tmp.jpg');
+  imwrite(img,'tmp.jpg','Quality',compression); 
+  %salva uma imagem temporaria, comprimindo-a com uma certa taxa de qualidade
+  img_ela = imread('tmp.jpg'); %lê a imagem salva
 
 
   %faz a subtração da imagem original com a imagem comprimida para observar as diferenças geradas pela compressao
   output = (abs(double(img)-double(img_ela))*15);
 
+  %transforma a saída em grayscale;
   output=mean(output,3);
   
 endfunction
 
 
-
-
 img = imread("images/4.jpg");
-img_metadata = imfinfo("images/4.jpg");
-img_metadata
+img_metadata = imfinfo("images/4.jpg"); %lê os metadados da imagem
+img_metadata %análise de metadados da imagen
+output = ela(img, 70); %chama a função de error level analysis
 
-output = ela(img, 70);
-figure, imagesc(img);
-figure, imagesc(output);
+figure;
+subplot(2, 2, 1), imagesc(img), title("Imagem Original"); 
+%mostra as imagens na tela usando imagesc
+subplot(2, 2, 2), imagesc(output), title("ELA - Imagem Original");
 
 
 img_edited = imread("images/4-edited.jpg");
@@ -39,6 +48,5 @@ edited_metadata
 
 output = ela(img_edited, 70);
 
-figure, imagesc(img_edited);
-
-figure, imagesc(output);
+subplot(2, 2, 3), imagesc(img_edited), title("Imagem Editada");
+subplot(2, 2, 4), imagesc(output), title("ELA - Imagem Editada");
